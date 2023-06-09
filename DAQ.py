f0cb17b7fe9002741dd9e7bace6a23fa729b9db4
kristofer0907 <94832139+kristofer0907@users.noreply.github.com>
2023-06-09 14:55:07 -0700

    Added code for the amount of time it takes to acquire readings for voltage
    Added some TODO's as well

--- DAQ.py
+++ DAQ.py
@@ -13,14 +13,14 @@ def set_analog_output(analog_input_channel,voltage_level,rate,num_samples):
 
 def read_voltage(analog_input_channel,SAMPLE_AMOUNT):    
     with nidaqmx.Task() as task:
-        #start_time = time.time()
         task.ai_channels.add_ai_voltage_chan(analog_input_channel)
-        #task.ai_channels.add_ai_current_chan(analog_input_channel)
+        start_time = time.perf_counter()
         data = task.read(number_of_samples_per_channel= SAMPLE_AMOUNT)
+        end_time = time.perf_counter()
+        elapsed_time= end_time-start_time
         average = np.mean(data)
-        #end_time = time.time()-start_time
-        #print(end_time)
-        return data
+       
+        return data, elapsed_time
     
 def read_current(analog_input_channel,SAMPLE_AMOUNT,period):    
     with nidaqmx.Task() as task:
@@ -41,6 +41,8 @@ def plotter(I_data,V_data):
 
 
 
+# TODO: Change into rms values for better I-V reading
+# TODO: Make the two data acquisition for voltage and current into a single function
 start_time = time.time()
 VOLTAGE_LEVEL = 0.0005
 analog_input_channel = "cDAQ1Mod2/ai0"  # Replace with the appropriate channel name for your setup
@@ -49,11 +51,14 @@ SAMPLE_AMOUNT = 1200
 FREQUENCY = 1000
 set_analog_output(analog_output_channel,VOLTAGE_LEVEL,FREQUENCY,SAMPLE_AMOUNT)
 time.sleep(1)
-voltage_reading = read_voltage(analog_input_channel,SAMPLE_AMOUNT)
+voltage_reading,acquisition_time_voltage = read_voltage(analog_input_channel,SAMPLE_AMOUNT)
 period = (2*math.pi)/FREQUENCY
+
 current_reading = read_current(analog_input_channel,SAMPLE_AMOUNT,period)
 #print(f"Voltage reading: {voltage_reading} V")
+print(f"Analog input acquisition time: {acquisition_time_voltage} seconds")
 #print(f"Current reading: {current_reading} A")
+
 print(len(voltage_reading))
 print(len(current_reading))
 print()
